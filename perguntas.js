d2524149212b5e8005e67613035b9ca152a5b522
braga0192007 <guibragaandrade2007@gmail.com>
2024-11-14 14:23:40 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Futebol',
+    'Qual o melhor time do Brasil?',
+    'O Sport Club Corinthians Paulista'
+)
+
+criaCartao(
+    'Games',
+    'Qual o ultimo jogo lançado da Activison?',
+    'Foi o jogo Call Of Duty: Black Ops 6, lançado dia 25/10/2024'
+)
+
+criaCartao(
+    'Futebol',
+    'Quantas bolas de ouro o Cristiano Ronaldo tem?',
+    'O Cristiano Ronaldo tem 5 bolas de ouro'
+)
+
+criaCartao(
+    'Futebol',
+    'Quantas copas do mundo o Brasil tem?',
+    'O Brasil tem 5 copas do mundo'
+)
